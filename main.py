a46b2643973f6baff0b511dadf69086a0de5d80a
HannaCSEMachine <hcf0018@unt.edu>
2020-04-23 17:06:51 -0500

    added nondeadlocked text and started work on detection algorithm

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import numpy as np
 
 # This class holds the matrix object, with all information from the text file
 class Matrix:
-
-    # Functions with two underscores are default functionalities of classes that you can define. 
+    # Functions with two underscores are default functionalities of classes that you can define.
     # __init__() is the constructor method. I pass all needed information from the text file, and assign the values to values of the object
     def __init__(self, numProcesses, numResources, units, matrix):
         #self. refers to data that is a part of this object
@@ -12,12 +11,17 @@ class Matrix:
         self.units = units
         self.matrix = matrix
         # Print statement to verify the object was created correctly
-        print(f"Matirx Created\nNumber of Processes: {self.numP}\nNumber of Resources: {self.numR}\nResource Units per Row: {self.units}\nMatrix:\n {self.matrix}")
-    
+        print(f"Matrix Created\nNumber of Processes: {self.numP}\nNumber of Resources: {self.numR}\nResource Units per Resource: {self.units}")
+
     # The __str__ method is how the object will be treated if referenced as a string, I chose to use the string representation for the matrix.
     # This allows us to print the object, and get just the matrix as our result
     def __str__(self):
         return np.array_str(self.matrix)
+
+    def findReducable(self):
+        for x in range(0,self.numP):
+            for y in range(self.numP, self.numP+self.numR):
+                print(f"{x} {y}");
 
 # This function parses the text file provided
 def parse(filename):
@@ -35,7 +39,7 @@ def parse(filename):
                     # Cases to parse out the information
                     # If we find a "=" character in the string (-1 is the return value for not found), than we know it's either the number of processes, or resources
                     if line.find("=") != -1:
-                        # .split will break a string into parts based on the argument provided. So this will split the string before and after every "="   
+                        # .split will break a string into parts based on the argument provided. So this will split the string before and after every "="
                         x = line.split("=")
                         if x[0] == "num_processes":
                             # We cast it as an int here
@@ -61,13 +65,15 @@ def parse(filename):
     return matrixObject
 
 def main():
-    # Un-commenting this line will allow the user to input a file upon runtime. 
-    #filename = input("Enter the filename/path: ")
+    # Un-commenting this line will allow the user to input a file upon runtime.
+    filename = input("Enter the filename/path: ")
     # A hardcoded file destination, for ease of testing
-    filename = "C:/Users/DanielBatesJ/Desktop/UNT/Spring 2020/4600/Project2/dead.txt"
+    # filename = "C:/Users/DanielBatesJ/Desktop/UNT/Spring 2020/4600/Project2/dead.txt"
     matrix = parse(filename)
     # I setup the class so that printing our object will print the matrix for ease of use
 
-    # The print(f"") adapts python's print to act as a more C style print function. Using {} to place variables. 
-    print(f"Testing the default print method of our object\n{matrix}")
-main()
+    # The print(f"") adapts python's print to act as a more C style print function. Using {} to place variables.
+    print(matrix);
+    matrix.findReducable();
+
+main();
